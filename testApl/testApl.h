0265844fb9709cff8a7cf592b58e098091a8cdb3
kuwamurk <kayoko.kuwamura@justsystems.com>
2019-09-24 11:30:05 +0900

    仕様1：見出し語Editに「上手」と出力

---
+++ testApl/testApl.h
@@ -0,0 +1,32 @@
+
+// testApl.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです。
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'stdafx.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CtestAplApp:
+// このクラスの実装については、testApl.cpp を参照してください。
+//
+
+class CtestAplApp : public CWinApp
+{
+public:
+	CtestAplApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CtestAplApp theApp;
